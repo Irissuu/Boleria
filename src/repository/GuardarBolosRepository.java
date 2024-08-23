1d6b8818b1dc7e3a2b9f112b48462681a21836da
irissuu <iriswinx2@gmail.com>
2024-08-23 10:30:03 -0300

    criando onde guardar os bolos

---
+++ src/repository/GuardarBolosRepository.java
@@ -0,0 +1,9 @@
+package repository;
+import models.Bolos;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuardarBolosRepository implements BolosRepository {
+    private List<Bolos> bolosList = new ArrayList<>();
+
+}
